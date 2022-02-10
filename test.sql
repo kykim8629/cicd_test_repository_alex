265b1b3301b29d86cea065bb6e268cf8a921c906
kykim8629 <74532423+kykim8629@users.noreply.github.com>
2022-02-11 08:56:03 +0900

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,5 +1,5 @@
-create table cj-olive-young-poc.dataplatform_poc_dataset_01.cicd_test_alex(id int64);
+create table if not exists cj-olive-young-poc.dataplatform_poc_dataset_01.cicd_test_alex(id int64);
 
-insert into cj-olive-young-poc.dataplatform_poc_dataset_01.cicd_test_alex (id) values(1);
+insert into cj-olive-young-poc.dataplatform_poc_dataset_01.cicd_test_alex (id) values(2);
 
 drop table cj-olive-young-poc.dataplatform_poc_dataset_01.cicd_test_alex;
