5bab8facd6f0610fd6eaf99853d5b8fa19e45b67
kykim8629 <74532423+kykim8629@users.noreply.github.com>
2022-02-11 08:46:39 +0900

    Update cicd_adapt_bigquery_proc.py

--- cicd_adapt_bigquery_proc.py
+++ cicd_adapt_bigquery_proc.py
@@ -1,10 +1,38 @@
-from airflow import DAG
+import os
+import codecs
 from datetime import datetime
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCheckOperator,
+    BigQueryCreateEmptyDatasetOperator,
+    BigQueryCreateEmptyTableOperator,
+    BigQueryDeleteDatasetOperator,
+    BigQueryGetDataOperator,
+    BigQueryInsertJobOperator,
+    BigQueryIntervalCheckOperator,
+    BigQueryValueCheckOperator,
+)
+from airflow.operators.dummy import DummyOperator
+from google.cloud import storage
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/airflow/gcs/data/key/cj-olive-young-poc-7d67896f3d5c.json"
 
 default_args = {
-    'start_date' : datetime(2021,1,1)
+    'owner': 'airflow',
+    'start_date' : datetime(2021,1,1),
+    'email_on_failure': False,
+    'email_on_retry': False,
 }
+
+
+def _extracting_sql() :
+    client = storage.Client()
+    bucket = client.get_bucket('asia-northeast3-cicd-airflo-b991dc6b-bucket')
+    blob = bucket.get_blob('data/test.sql')
+    sqlquery = blob.download_as_string()
+    str_sqlquery = codecs.decode(sqlquery, 'UTF-8')
+
+    return str_sqlquery
 
 with DAG(dag_id='adapting_query_to_bigquery',
          schedule_interval=None,
@@ -12,4 +40,19 @@ with DAG(dag_id='adapting_query_to_bigquery',
          tags=['cicd_test_alex'],
          catchup=False
          ) as dag :
-    pass
+    t1= DummyOperator(task_id='start',
+                          dag=dag)   
+    t2= BigQueryInsertJobOperator(task_id='executing_sql_on_bigquery',
+                                  gcp_conn_id='cicd_bigquery_conn',
+                                  configuration={
+                                                "query": {
+                                                    "query": _extracting_sql(),
+                                                    "useLegacySql": False
+                                                }
+                                            }
+                                 )
+
+    t3= DummyOperator(task_id='end',
+                      dag=dag)
+
+    t1 >> t2 >> t3
